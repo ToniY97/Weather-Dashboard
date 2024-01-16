4081fdb70eaa32f9ba7df39ab65f4cdf205ec457
Toni97 <ayovcheva75@gmail.com>
2024-01-16 15:15:35 +0000

    working on search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
 $(document).ready(function () {
     const apiKey = '2699e46eb89b15f734f582dc3b354080';
     const historyKey = 'weatherSearchHistory';
-  
+
     // Retrieve search history from local storage or initialize an empty array
     let searchHistory = JSON.parse(localStorage.getItem(historyKey)) || [];
-  
+
     // Display search history on page load
     displaySearchHistory();
 
     // Event listener for the search form submission
     $('#search-form').submit(function (event) {
         event.preventDefault();
-        
+
         // Get the city name from the input field
         const cityName = $('#search-input').val().trim();
-        
+
         // Fetch weather data for the given city
         getWeatherData(cityName);
-      });
+    });
+
+    $('#history').on('click', '.list-group-item', function () {
+        const cityName = $(this).text().trim();
+        getWeatherData(cityName);
+    });
+
+    
